643e4fd168723edf0648f2c94aea236b6ceba757
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-22 11:16:01 +0700

    jika font lebih besar maka hingga akhir

--- circularqueues_004/circularqueues_004.cpp
+++ circularqueues_004/circularqueues_004.cpp
@@ -80,3 +80,9 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
