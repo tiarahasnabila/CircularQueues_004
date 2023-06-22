f815635a6bef82ad2cf8b30801526c27241578b1
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-22 11:02:06 +0700

    elemen yang sudah dihapus dikembalikan ke awal array

--- circularqueues_004/circularqueues_004.cpp
+++ circularqueues_004/circularqueues_004.cpp
@@ -51,4 +51,13 @@ public:
 			FRONT = -1;
 				REAR = -1;
 		}
+		else {
+			//jika elemen yang dihapus berada diposisi terakhir array,kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
 	
