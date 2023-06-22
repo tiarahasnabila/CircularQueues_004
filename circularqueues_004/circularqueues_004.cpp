4a9b47026dc1bec094592777ec08db5a762933e4
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-22 11:15:00 +0700

    iterasi dari font hingga rear

--- circularqueues_004/circularqueues_004.cpp
+++ circularqueues_004/circularqueues_004.cpp
@@ -72,4 +72,11 @@ public:
 
 		cout << "\nElements is the queue are: \n";
 
-		
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
