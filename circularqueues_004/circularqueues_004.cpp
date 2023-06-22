1d1a5cc2075cb913c82dc21de2a2f088329f57d8
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-22 11:33:46 +0700

    menampilkan menu

--- circularqueues_004/circularqueues_004.cpp
+++ circularqueues_004/circularqueues_004.cpp
@@ -103,3 +103,15 @@ int main() {
 	char ch;
 
 
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display Values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter yoir choice (1-4):" << endl;
+			cin >> ch;
+			cout << endl;
+
